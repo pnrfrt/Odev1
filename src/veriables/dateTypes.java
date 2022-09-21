242475906f2627fce8cd694b557f9b4d9c7ad20e
pinar.firat@etiya.com <pinar.firat@etiya.com>
2022-09-21 17:37:38 +0300

    typescommit

---
+++ src/veriables/dateTypes.java
@@ -0,0 +1,12 @@
+package veriables;
+
+public class dateTypes {
+
+	public static void main(String[] args) {
+		String kelime = "ahmet";
+		Double sayi = 12.4;
+		
+		System.out.println(sayi);
+	}
+
+}
