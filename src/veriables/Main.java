98fc3faa59fa31e3d303ac4c75b0d36fc0784a73
pinar.firat@etiya.com <pinar.firat@etiya.com>
2022-09-21 17:13:06 +0300

    commit

---
+++ src/veriables/Main.java
@@ -0,0 +1,12 @@
+package veriables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int sayi = 10;
+		String mesaj = "öğrenci sayısı :";
+		System.out.println( mesaj +sayi);
+	}
+
+}
